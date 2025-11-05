8b44d687ba936dfa7edf94eb73ade3e15f4db05b
ansh-bindal <anshbindal777@gmail.com>
2025-11-05 19:21:15 +0530

    Create Q4.cpp

---
+++ Assignment 6/Q4.cpp
@@ -0,0 +1,51 @@
+#include<iostream>
+using namespace std;
+
+class node{
+public:
+char data;
+node* next;
+node* prev;
+node(char d='0'){
+data=d;
+next=NULL;
+prev=NULL;
+}
+};
+
+bool check(node* &head){
+if(head==NULL)return true;
+node* start=head;
+node* end=head;
+while(end->next!=NULL)
+end=end->next;
+while(start!=end&&end->next!=start){
+if(start->data!=end->data)
+return false;
+start=start->next;
+end=end->prev;
+}
+return true;
+}
+
+int main(){
+node* a=new node('l');
+node* b=new node('e');
+node* c=new node('v');
+node* d=new node('e');
+node* e=new node('l');
+a->next=b;
+b->prev=a;
+b->next=c;
+c->prev=b;
+c->next=d;
+d->prev=c;
+d->next=e;
+e->prev=d;
+node* head=a;
+if(check(head))
+cout<<"Palindrome\n";
+else
+cout<<"Not a Palindrome\n";
+return 0;
+}
